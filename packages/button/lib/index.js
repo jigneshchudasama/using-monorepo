62ef88c1259dacc689f1c082e4154a23805b3bc5
Jignesh Chudasama <jignesh.chudasama@retailio.in>
2021-05-25 17:18:18 +0530

    Test component added

---
+++ packages/button/lib/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var CustomButton = _styledComponents["default"].button.withConfig({
+  displayName: "src__CustomButton",
+  componentId: "sc-132cw10-0"
+})(["background:red;color:#fff;border-radius:4px;cursor:pointer;font-size:1rem;font-weight:300;padding:9px 36px;"]);
+
+var _default = CustomButton;
+exports["default"] = _default;
---
+++ packages/button/src/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import CustomButton from '.';
+
+export default {
+  component: CustomButton,
+  title: 'Design System/Button'
+};
+
+export const primary = () => <CustomButton>{'Button'}</CustomButton>;
